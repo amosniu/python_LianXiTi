a7c9209537f9f28499a0a2fc591ba8802eb19e0c
amosniu <niuamos@gmail.com>
2018-12-03 10:24:39 +0800

    将json数据转化成python数据

--- use_json.py
+++ use_json.py
@@ -9,5 +9,28 @@ def pythonToJson():
     jsonData = json.dumps(d)
     print(jsonData)
 
+def jsonToPython():
+    ''' 将json数据转化成python数据 '''
+    jsonData = '''
+    {
+    "name": "Python书籍",
+    "origin_price": 66,
+    "pub_date": "2018-4-14 17:00:00",
+    "store": ["京东", "淘宝"],
+    "author": ["张三", "李四", "Jhone"],
+    "is_valid": true,
+    "is_sale": false,
+    "meta": {
+        "isbn": "abc-123",
+        "pages": 300
+    },
+    "desc": null
+    }
+    '''
+    pythonData = json.loads(jsonData)
+    print(pythonData)
+
 if __name__ == '__main__':
     pythonToJson()
+    print('---------------------------')
+    jsonToPython()
