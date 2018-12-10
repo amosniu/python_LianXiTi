51558ed94a5854e36b044d8d63231d22cbe000ff
amosniu <niuamos@gmail.com>
2018-12-10 17:20:57 +0800

    递归实现这帮小兔崽子

--- digui.py
+++ digui.py
@@ -19,3 +19,17 @@ def factorial2(number2):
 number2 = int(input('请输入一个正整数：'))
 result2 = factorial2(number2)
 print('%d的阶乘是：%d' %(number2,result2))
+
+#递归版本（这帮小兔崽子）
+def fab(number3):
+    if number3 < 1:
+        return -1
+    if number3 == 1 or number3 == 2:
+        return 1
+    else:
+        return fab(number3-1)+fab(number3-2)
+
+nummber3 = int(input("请输入一个正整数："))
+result3 = fab(nummber3)
+if result3 != -1:
+    print('第%d天时共有%d对兔子' %(nummber3,result3))
