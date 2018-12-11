e8c74786313e36813575f4100e6e6a817c231fb9
amosniu <niuamos@gmail.com>
2018-12-11 09:29:29 +0800

    递归这帮小兔崽子优化

--- digui.py
+++ digui.py
@@ -21,15 +21,15 @@ result2 = factorial2(number2)
 print('%d的阶乘是：%d' %(number2,result2))
 
 #递归版本（这帮小兔崽子）
-def fab(number3):
+def factorial3(number3):
     if number3 < 1:
         return -1
     if number3 == 1 or number3 == 2:
         return 1
     else:
-        return fab(number3-1)+fab(number3-2)
+        return factorial3(number3-1)+factorial3(number3-2)
 
 nummber3 = int(input("请输入一个正整数："))
-result3 = fab(nummber3)
+result3 = factorial3(nummber3)
 if result3 != -1:
     print('第%d天时共有%d对兔子' %(nummber3,result3))
